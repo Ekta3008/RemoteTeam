dc74163134dc5fda67250bb87be00c9b5043d32c
dm00474408 <dm00474408@INPUHJPC09773.ATT.TechMahindra.com>
2017-04-13 18:03:56 +0530

    Initial commit

--- REMote/src/remote_package/Rm_Class.java
+++ REMote/src/remote_package/Rm_Class.java
@@ -5,7 +5,9 @@ public class Rm_Class {
 	public static void main(String[] args) {
 		System.out.println("Ekta-1");
 		System.out.println("Deepak-1");
-		System.out.println("Deepak-2");
+		System.out.println("Ravi-1");
+		System.out.println("Ravi-2");
+		System.out.println("Ravi-3");
 	}
 
 }
